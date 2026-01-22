d2cc7083a71429b84dcde2e14a2bcdc7f89f8318
Arjen10 <thistime833@gmail.com>
2026-01-22 20:32:11 +0800

    bug fix：控制台中文乱码

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "src/sl/app.hpp"
 
 int main(int argc, char* argv[]) {
+
+#ifdef _WIN32
+    // 控制台切换为 UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
     app a(argc, argv);
     return a.run();
 }
